d6a4a3f745a9f854fcc09ec2f0cb6841f2a69f45
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2024-12-07 15:22:20 +0700

    7/12/2024

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Learn from './Learn'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Learn></Learn>
     </div>
   );
 }
---
+++ myapp/src/Learn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Learn.css'
+
+
+const Learn = () => {
+    return (
+        <div className='inner'>
+            <div className='conic'>
+            </div>
+            <div className='content'>
+            <h1 >DOMO DANCE PARTY!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Learn;
